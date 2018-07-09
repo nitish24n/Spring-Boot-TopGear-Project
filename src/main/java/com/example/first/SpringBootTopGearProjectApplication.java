b22c448f27804eddd1b621529a4467f3e7f0ff29
nitish24n <nitish24m@gmail.com>
2018-07-09 20:21:16 +0530

    initial commit

---
+++ src/main/java/com/example/first/SpringBootTopGearProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTopGearProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTopGearProjectApplication.class, args);
+	}
+}
